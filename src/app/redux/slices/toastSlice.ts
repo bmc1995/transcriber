20b81d818f82fcb1b38585e336433d0ce0e84c65
Billy McCrackin <44419353+bmc1995@users.noreply.github.com>
2024-04-07 12:05:38 -0600

    fix(toast): add types

--- src/app/redux/slices/toastSlice.ts
+++ src/app/redux/slices/toastSlice.ts
@@ -1,5 +1,5 @@
-import { ColorPaletteProp, VariantProp } from "@mui/joy";
-import { createSlice } from "@reduxjs/toolkit";
+import { ColorPaletteProp, VariantProp } from '@mui/joy';
+import { createSlice } from '@reduxjs/toolkit';
 
 export interface ToastAlert {
   id: string;
@@ -20,22 +20,25 @@ type ToastRemoveAction = {
 };
 
 export const toastSlice = createSlice({
-  name: "toast",
+  name: 'toast',
   initialState: {
     toastQueue: <ToastAlert[]>[],
   },
   reducers: {
-    addToast: (state, action: ToastAddAction) => {
+    addToast: (
+      state: {
+        toastQueue: { id: string; duration: number; msg: string; color: ColorPaletteProp; variant: VariantProp }[];
+      },
+      action: ToastAddAction,
+    ) => {
       state.toastQueue.push({
         ...action.payload,
       });
     },
-    removeToast: (state, action: ToastRemoveAction) => {
+    removeToast: (state: { toastQueue: ToastAlert[] }, action: ToastRemoveAction) => {
       return {
         ...state,
-        toastQueue: state.toastQueue.filter(
-          (toast) => toast.id != action.payload
-        ),
+        toastQueue: state.toastQueue.filter((toast: { id: string }) => toast.id != action.payload),
       };
     },
   },
